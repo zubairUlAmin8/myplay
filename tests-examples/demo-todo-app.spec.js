7bb2921bde9e35ecd3c4123fb04c7df1ca3c9eb3
Zubair Ul Amin <zubairulamin8@gmail.com>
2023-08-31 14:36:52 +0500

    check trigger

--- tests-examples/demo-todo-app.spec.js
+++ tests-examples/demo-todo-app.spec.js
@@ -37,6 +37,31 @@ test.describe('New Todo', () => {
 
     await checkNumberOfTodosInLocalStorage(page, 2);
   });
+  test('should allow me to add todo items', async ({ page }) => {
+    // create a new todo locator
+    const newTodo = page.getByPlaceholder('What needs to be done?');
+
+    // Create 1st todo.
+    await newTodo.fill(TODO_ITEMS[0]);
+    await newTodo.press('Enter');
+
+    // Make sure the list only has one todo item.
+    await expect(page.getByTestId('todo-title')).toHaveText([
+      TODO_ITEMS[0]
+    ]);
+
+    // Create 2nd todo.
+    await newTodo.fill(TODO_ITEMS[1]);
+    await newTodo.press('Enter');
+
+    // Make sure the list now has two todo items.
+    await expect(page.getByTestId('todo-title')).toHaveText([
+      TODO_ITEMS[0],
+      TODO_ITEMS[1]
+    ]);
+
+    await checkNumberOfTodosInLocalStorage(page, 2);
+  });
 
   test('should clear text input field when an item is added', async ({ page }) => {
     // create a new todo locator
